6f8abf4f6a3b93c37a0a56e3b22fd0c9ed837c78
David Thong Nguyen <dev@davidtn.com>
2019-08-31 14:33:35 +0700

    Idea is right, need some debugging

--- src/app/sorts/selection/selection.component.ts
+++ src/app/sorts/selection/selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 /**
  * The selection sort algorithm sorts an array by repeatedly finding the minimum element
@@ -13,24 +14,49 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./selection.component.scss']
 })
 export class SelectionComponent implements OnInit {
-  @Input() arr = [64, 25, 12, 22, 11];
+  @Input() input = [64, 25, 12, 22, 11];
+  result = new BehaviorSubject<number[]>(this.input);
+  numbersSorted = 0;
   constructor() {}
 
-  ngOnInit() {
-    console.log(this.sort(this.arr));
+  ngOnInit() {}
+
+  onClick() {
+    const arr = Object.assign([], this.result.value);
+    if (this.numbersSorted >= arr.length) {
+      return;
+    }
+
+    const minInd = this.selectMinInd(arr.slice(this.numbersSorted, arr.length));
+
+    // swap
+    const temp = arr[minInd];
+    arr[minInd] = arr[this.numbersSorted];
+    arr[this.numbersSorted] = temp;
+    this.numbersSorted++;
+    this.result.next(arr);
+  }
+
+  selectMinInd(unsortedSubArr: number[]) {
+    let minInd = 0;
+    for (
+      let unsortedInd = 0;
+      unsortedInd < unsortedSubArr.length;
+      unsortedInd++
+    ) {
+      if (unsortedSubArr[unsortedInd] < unsortedSubArr[minInd]) {
+        minInd = unsortedInd;
+      }
+    }
+    return minInd;
   }
 
   private sort(arr: number[]) {
-    const result = Object.assign([], arr);
+    const result = arr;
     const n = result.length;
 
     for (let i = 0; i < n - 1; i++) {
-      let minInd = i;
-      for (let unsortedInd = i + 1; unsortedInd < n; unsortedInd++) {
-        if (result[unsortedInd] < result[minInd]) {
-          minInd = unsortedInd;
-        }
-      }
+      const minInd = this.selectMinInd(result.slice(i + 1, n));
 
       // swap
       const temp = result[minInd];
