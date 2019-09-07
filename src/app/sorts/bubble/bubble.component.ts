b7ea31851537fa686e0365295b4c610b15b479e9
David Thong Nguyen <dev@davidtn.com>
2019-09-07 12:30:43 +0700

    Basic bubble sort done

--- src/app/sorts/bubble/bubble.component.ts
+++ src/app/sorts/bubble/bubble.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { SortComponent } from '../sort-component';
 import { SortItem } from 'src/app/classes/sort-item';
 import { RandomNumService } from 'src/app/services/random-num.service';
@@ -10,14 +10,41 @@ import { RandomNumService } from 'src/app/services/random-num.service';
 })
 export class BubbleComponent extends SortComponent
   implements OnInit, OnDestroy {
-  constructor(private randomNumService: RandomNumService) {
-    super(randomNumService);
+  constructor(randomNumService: RandomNumService, cd: ChangeDetectorRef) {
+    super(randomNumService, cd);
+  }
+
+  ngOnInit() {
+    this.sampleSize = 10;
+    this.onChangeSampleSize();
   }
 
   /*************************************************************************/
   /**************************** BUBBLE SORT ONLY ***************************/
   /*************************************************************************/
   sort(input: SortItem<number>[], speed: number) {
-    throw new Error('Method not implemented.');
+    let result = this.input;
+    console.log([...result]);
+
+    // originally O(n^2) because looping n times per n elements
+    for (let i = 0; i < result.length; i++) {
+      console.log(result[i]);
+      // not sure why result.length - i - 1
+      for (let j = 0; j < result.length - 1; j++) {
+        console.log(result[j].value + ' vs ' + result[j + 1].value);
+        if (result[j].value > result[j + 1].value) {
+          console.log('should swap');
+          const temp = result[j];
+          result[j] = result[j + 1];
+          result[j + 1] = temp;
+          console.log([...result]);
+        }
+      }
+    }
+    console.log(result);
+
+    return;
   }
+
+  swap(a, b) {}
 }
