fc899f5dc46cbb815e4259e07c4c455030dc5ccf
David Thong Nguyen <dev@davidtn.com>
2019-09-10 22:22:17 +0700

    Setting up logic, still need to check k

--- src/app/sorts/insertion/insertion.component.ts
+++ src/app/sorts/insertion/insertion.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 import { SortComponent } from '../sort-component';
 import { SortItem } from 'src/app/classes/sort-item';
 import { RandomNumService } from 'src/app/services/random-num.service';
+import { SortData } from 'src/app/classes/sort-data';
 
 @Component({
   selector: 'app-insertion',
@@ -19,6 +20,25 @@ export class InsertionComponent extends SortComponent {
   /***************************** INSERTION SORT ****************************/
   /*************************************************************************/
   protected sort(input: SortItem<number>[]): void {
-    throw new Error('Method not implemented.');
+    const currentResult: SortData = {
+      data: this.deepCopy(input),
+      sorted: 0
+    };
+
+    for (let j = 0; j < currentResult.data.length; j++) {
+      // let key = j;
+      let k = j - 1;
+
+      console.log('k', currentResult.data[k], 'j', currentResult.data[j]);
+      while (
+        k >= 0 &&
+        currentResult.data[k].value > currentResult.data[j].value
+      ) {
+        currentResult.data[k + 1] = currentResult.data[k];
+        k--;
+        this.printArray(currentResult.data);
+      }
+      currentResult.data[k + 1] = currentResult.data[j];
+    }
   }
 }
