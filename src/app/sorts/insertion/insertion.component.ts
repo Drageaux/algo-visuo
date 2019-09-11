5b924b94ada6e22c84e10d42b8f09b778ac26640
David Thong Nguyen <dev@davidtn.com>
2019-09-11 10:18:04 +0700

    Need to highlight

--- src/app/sorts/insertion/insertion.component.ts
+++ src/app/sorts/insertion/insertion.component.ts
@@ -3,6 +3,7 @@ import { SortComponent } from '../sort-component';
 import { SortItem } from 'src/app/classes/sort-item';
 import { RandomNumService } from 'src/app/services/random-num.service';
 import { SortData } from 'src/app/classes/sort-data';
+import { SortStatus } from 'src/app/classes/sort-status.enum';
 
 @Component({
   selector: 'app-insertion',
@@ -25,20 +26,21 @@ export class InsertionComponent extends SortComponent {
       sorted: 0
     };
 
-    for (let j = 0; j < currentResult.data.length; j++) {
-      // let key = j;
-      let k = j - 1;
+    for (let a = 1; a < currentResult.data.length; a++) {
+      let temp = currentResult.data[a];
+      temp.status = SortStatus.SORTING;
+      this.pushState(currentResult);
+      let b = a - 1;
 
-      console.log('k', currentResult.data[k], 'j', currentResult.data[j]);
-      while (
-        k >= 0 &&
-        currentResult.data[k].value > currentResult.data[j].value
-      ) {
-        currentResult.data[k + 1] = currentResult.data[k];
-        k--;
-        this.printArray(currentResult.data);
+      console.log('a', currentResult.data[a], 'b', currentResult.data[b]);
+      while (b >= 0 && currentResult.data[b].value > temp.value) {
+        currentResult.data[b + 1] = currentResult.data[b];
+        b--;
       }
-      currentResult.data[k + 1] = currentResult.data[j];
+      // by this time, if b is decremented all the way to -1
+      // temp is inserted at the beginning
+      currentResult.data[b + 1] = temp;
+      this.printArray(currentResult.data);
     }
   }
 }
