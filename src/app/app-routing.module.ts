5066e0d748b56051d5907587f80890c6bd83e388
David Thong Nguyen <dev@davidtn.com>
2019-10-07 11:58:46 -0700

    Lazy load SortsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   },
   {
     path: 'sorts',
-    component: SortsComponent
+    loadChildren: () =>
+      import('./sorts/sorts.module').then(mod => mod.SortsModule)
   },
   {
     path: '**',
