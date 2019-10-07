beeb685b8b269738e8a11ff2f735f3a7e81670af
David Thong Nguyen <dev@davidtn.com>
2019-10-07 16:31:20 -0700

    2D grid blocks

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphs.component.scss']
 })
 export class GraphsComponent implements OnInit {
+  cols = 50;
+  rows = 50;
+  data = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    for (let y = 0; y < this.rows; y++) {
+      const row = [];
+      for (let x = 0; x < this.cols; x++) {
+        row.push({ x, y });
+      }
+      this.data.push(row);
+    }
   }
-
 }
